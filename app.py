1273a210923a5c269603177534fe133dfa9e9ae4
RaVaNAmit <amit.smriti.1999@gmail.com>
2024-10-10 22:16:54 +0530

    Pipeline Updated

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/1')
+def hello1():
+    return 'This is Path 1?'
+
+@app.route('/2')
+def hello2():
+    return 'This is Path 2?'
+
+@app.route('/3')
+def hello3():
+    return 'This is Path 3?'
 
 if __name__ == "__main__":
     app.run()
